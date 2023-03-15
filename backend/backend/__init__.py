d5ac5bbd7b855e8aecf308e2ce21275472f8f598
The Cheaterman <the.cheaterman@gmail.com>
2023-03-15 16:06:49 +0100

    Use an app factory to facilitate config injection

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-@app.route('/')
-def api_index():
-    return {'message': 'Hello!'}
+def create_app(config=None):
+    app = Flask(__name__)
+
+    if config:
+        app.config.from_mapping(config)
+
+    @app.route('/')
+    def api_index():
+        return {'message': 'Hello!'}
+
+    return app
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,17 +1,17 @@
 import pytest
 
-from backend import app
+from backend import create_app
 
 
 @pytest.fixture(scope='session')
-def app_context():
+def app():
+    app = create_app({'TESTING': True})
+
     with app.app_context():
         yield app
 
 
 @pytest.fixture
-def client(app_context):
-    app.config['TESTING'] = True
-
+def client(app):
     with app.test_client() as client:
         yield client
