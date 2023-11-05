3575c36894a67630df8a5caa6b4139781168b102
The Cheaterman <the.cheaterman@gmail.com>
2023-11-05 20:40:39 +0100

    Make proxy listen to :80 for frontend (no UNIX socket support yet)
    See also: https://github.com/unjs/httpxy/issues/22

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    routeRules: {
+      '/api/**': { proxy: 'http://proxy:80/api/**' },
+    },
+  },
 })
